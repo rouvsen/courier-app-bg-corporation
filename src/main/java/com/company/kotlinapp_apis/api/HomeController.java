7b0b24a7b68afc0792d4d996762c366abdd950c6
rovshan <rouvsen@gmail.com>
2023-06-17 19:00:34 +0400

    updated.

--- src/main/java/com/company/kotlinapp_apis/api/HomeController.java
+++ src/main/java/com/company/kotlinapp_apis/api/HomeController.java
@@ -4,16 +4,17 @@ import com.company.kotlinapp_apis.api.login.model.Result;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping("/api/v1/home")
+@RequestMapping("/api")
 public class HomeController {
 
         @GetMapping("/home")
         public ResponseEntity<Result> homePage() {
             return ResponseEntity.status(HttpStatus.OK).body(Result.builder()
-                    .result("It is result...")
+                    .result("Here Home Page...")
                     .build());
         }
 
