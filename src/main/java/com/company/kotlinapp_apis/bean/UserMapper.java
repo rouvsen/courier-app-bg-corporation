e48c3c65c8e6e31711509b3c20a3f3dc9ea811f1
rovshan <rouvsen@gmail.com>
2023-06-18 17:40:26 +0400

    UserMapper added, updated

--- src/main/java/com/company/kotlinapp_apis/bean/UserMapper.java
+++ src/main/java/com/company/kotlinapp_apis/bean/UserMapper.java
@@ -3,8 +3,10 @@ package com.company.kotlinapp_apis.bean;
 import com.company.kotlinapp_apis.dto.UserDto.UserDto;
 import com.company.kotlinapp_apis.model.User;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
 @Mapper
+@Component
 public interface UserMapper {
 
     UserDto toDto(User user);
