95531f66711f275706a2fe19791b430ded228e7f
rovshan <rouvsen@gmail.com>
2023-06-19 22:17:55 +0400

    Shop E. - added

---
+++ src/main/java/com/company/kotlinapp_apis/model/shop/Shop.java
@@ -0,0 +1,42 @@
+package com.company.kotlinapp_apis.model.shop;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class Shop {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String latLong;
+
+    private String location;
+
+    private String phoneNumber;
+
+    private String oneSignal;//deviceId
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String createdDate;
+
+    private Boolean trash;
+
+    private String trashDate;
+}
