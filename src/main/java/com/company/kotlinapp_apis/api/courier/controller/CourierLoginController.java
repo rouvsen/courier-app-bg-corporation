5e16335974f27492249a2d163bab07cb171487af
rovshan <rouvsen@gmail.com>
2023-06-19 23:31:28 +0400

    Courier Login Controller added

---
+++ src/main/java/com/company/kotlinapp_apis/api/courier/controller/CourierLoginController.java
@@ -0,0 +1,45 @@
+package com.company.kotlinapp_apis.api.courier.controller;
+
+import com.company.kotlinapp_apis.dto.courier.CourierDto;
+import com.company.kotlinapp_apis.model.LoginRequest;
+import com.company.kotlinapp_apis.service.inter.courier.CourierServiceInter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/courier/login")
+public class CourierLoginController {
+
+    private final CourierServiceInter courierService;
+
+    public CourierLoginController(CourierServiceInter courierService) {
+        this.courierService = courierService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        String email = loginRequest.getEmail();
+        String password = loginRequest.getPassword();
+
+        CourierDto courierDto = courierService.findCourierByEmail(email);
+
+        System.out.println(courierDto);
+
+        if (courierDto == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("Invalid username or password");
+        }
+
+        if(courierDto.getPassword().equals(password)) {
+            return ResponseEntity.ok(courierDto);
+        }
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body("Invalid username or password");
+    }
+
+}
