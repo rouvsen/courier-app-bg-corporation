1aca25d0f9efc2b2b3f74e1ad97c2f388e059d3c
rovshan <rouvsen@gmail.com>
2023-06-19 22:17:46 +0400

    LoginRequest Entity for Logging - added

---
+++ src/main/java/com/company/kotlinapp_apis/model/LoginRequest.java
@@ -0,0 +1,17 @@
+package com.company.kotlinapp_apis.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class LoginRequest {
+
+    private String username;
+    private String password;
+
+}
