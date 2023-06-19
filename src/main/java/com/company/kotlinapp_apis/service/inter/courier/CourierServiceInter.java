903babaf5f15739e9601e86bbb8fb4af6bbe6e61
rovshan <rouvsen@gmail.com>
2023-06-19 22:17:25 +0400

    Courier ServiceInter added

---
+++ src/main/java/com/company/kotlinapp_apis/service/inter/courier/CourierServiceInter.java
@@ -0,0 +1,17 @@
+package com.company.kotlinapp_apis.service.inter.courier;
+
+import com.company.kotlinapp_apis.dto.courier.CourierDto;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface CourierServiceInter {
+
+    List<CourierDto> getAllCouriers();
+    CourierDto getCourierById(Long id);
+    ResponseEntity<CourierDto> createCourier(CourierDto courierDto);
+    CourierDto updateCourier(Long id, CourierDto courierDto);
+    Boolean deleteCourier(Long id);
+    Boolean isCourierEmailTaken(String email);
+
+}
