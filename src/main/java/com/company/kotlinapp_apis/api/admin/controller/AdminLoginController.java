6585d55efe92602ad77c4e74653acc97c4fc7e69
rovshan <rouvsen@gmail.com>
2023-06-19 22:16:27 +0400

    Admin Login added

---
+++ src/main/java/com/company/kotlinapp_apis/api/admin/controller/AdminLoginController.java
@@ -0,0 +1,37 @@
+package com.company.kotlinapp_apis.api.admin.controller;
+
+import com.company.kotlinapp_apis.dto.admin.AdminDto;
+import com.company.kotlinapp_apis.model.LoginRequest;
+import com.company.kotlinapp_apis.service.inter.admin.AdminServiceInter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/admin/login")
+public class AdminLoginController {
+
+    private final AdminServiceInter adminService;
+
+    public AdminLoginController(AdminServiceInter adminService) {
+        this.adminService = adminService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        String username = loginRequest.getUsername();
+        String password = loginRequest.getPassword();
+
+        AdminDto adminDto = adminService.findAdminByEmail(username);
+
+        if (adminDto == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+        }
+
+        return ResponseEntity.ok(adminDto);
+    }
+
+}
