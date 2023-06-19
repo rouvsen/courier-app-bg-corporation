641783aea3230a25bc4952b7100689259caa2b6e
rovshan <rouvsen@gmail.com>
2023-06-19 22:47:39 +0400

    Admin Repository - added

--- src/main/java/com/company/kotlinapp_apis/dao/admin/AdminRepository.java
+++ src/main/java/com/company/kotlinapp_apis/dao/admin/AdminRepository.java
@@ -4,8 +4,6 @@ import com.company.kotlinapp_apis.model.admin.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
     Admin findAdminByEmail(String email);
