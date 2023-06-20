3258f528a5fb430f43bc1e37b83944e62f7e5ed5
rovshan <rouvsen@gmail.com>
2023-06-21 00:00:10 +0400

    Admin RegisterController updated

--- src/main/java/com/company/kotlinapp_apis/api/admin/controller/AdminRegisterController.java
+++ src/main/java/com/company/kotlinapp_apis/api/admin/controller/AdminRegisterController.java
@@ -2,6 +2,7 @@ package com.company.kotlinapp_apis.api.admin.controller;
 
 import com.company.kotlinapp_apis.dto.admin.AdminDto;
 import com.company.kotlinapp_apis.service.impl.admin.AdminServiceImpl;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,6 +28,17 @@ public class AdminRegisterController {
     @GetMapping
     public List<AdminDto> getAllAdmins(){
         return adminServiceImpl.getAllAdmins();
+    }
+
+    //ID
+    @GetMapping("/{adminID}")
+    public ResponseEntity<?> getAdminById(@PathVariable("adminID") Long adminId){
+        AdminDto admin = adminServiceImpl.getAdminById(adminId);
+        if (admin != null) {
+            return ResponseEntity.ok(admin);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("admin doesn't exist with given id..");
     }
 
 }
