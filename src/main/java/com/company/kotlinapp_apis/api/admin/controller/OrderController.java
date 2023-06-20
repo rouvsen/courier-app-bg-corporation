a6eb37613ce72f15e567ad2baee32a0969b764e1
rovshan <rouvsen@gmail.com>
2023-06-21 00:01:04 +0400

    Order Controller updated

--- src/main/java/com/company/kotlinapp_apis/api/admin/controller/OrderController.java
+++ src/main/java/com/company/kotlinapp_apis/api/admin/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.company.kotlinapp_apis.api.admin.controller;
 
 import com.company.kotlinapp_apis.model.order.Order;
 import com.company.kotlinapp_apis.service.inter.order.OrderServiceInter;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,5 +25,26 @@ public class OrderController {
     public ResponseEntity<?> getAllOrders() {
         return ResponseEntity.ok(orderService.getAllOrders());
     }
+
+    @DeleteMapping("/orders/{orderId}")
+    public ResponseEntity<?> deleteOrderById(
+            @PathVariable(name = "orderId") Long orderId
+    ) {
+        orderService.removeById(orderId);
+        return ResponseEntity.ok("removed..");
+    }
+
+    @GetMapping("/orders/{orderId}")
+    public ResponseEntity<?> getOrderById(
+            @PathVariable(name = "orderId") Long orderId
+    ) {
+        Order order = orderService.getOrderById(orderId);
+        if(order != null) {
+            return ResponseEntity.ok(order);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("order doesn't exist with given id");
+    }
+
+
 
 }
