a1f8ab9b060fc186ca66697d2b40163340c89510
rovshan <rouvsen@gmail.com>
2023-06-21 00:01:47 +0400

    Shop RegisterController updated

--- src/main/java/com/company/kotlinapp_apis/api/shop/controller/ShopRegisterController.java
+++ src/main/java/com/company/kotlinapp_apis/api/shop/controller/ShopRegisterController.java
@@ -1,7 +1,9 @@
 package com.company.kotlinapp_apis.api.shop.controller;
 
+import com.company.kotlinapp_apis.dto.courier.CourierDto;
 import com.company.kotlinapp_apis.dto.shop.ShopDto;
 import com.company.kotlinapp_apis.service.impl.shop.ShopServiceImpl;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,6 +29,17 @@ public class ShopRegisterController {
     @GetMapping
     public List<ShopDto> getAllShops(){
         return shopServiceImpl.getAllShops();
+    }
+
+    //ID
+    @GetMapping("/{shopId}")
+    public ResponseEntity<?> getShopById(@PathVariable("shopId") Long shopId){
+        ShopDto shopDto = shopServiceImpl.getShopById(shopId);
+        if (shopDto != null) {
+            return ResponseEntity.ok(shopDto);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("shop doesn't exist with given id..");
     }
 
 }
