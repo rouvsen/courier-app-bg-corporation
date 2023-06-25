135b71675bd28c9fa64eaefcbfdf355dc0f57fb0
rovshan <rouvsen@gmail.com>
2023-06-26 01:36:55 +0400

    AdminYourDeptBalanceDet added

---
+++ src/main/java/com/company/kotlinapp_apis/model/finance/AdminYourDeptBalanceDet.java
@@ -0,0 +1,23 @@
+package com.company.kotlinapp_apis.model.finance;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AdminYourDeptBalanceDet {
+
+    @Id
+    private Long id;
+
+    private Integer price;
+    private String date;
+    private String time;
+}
