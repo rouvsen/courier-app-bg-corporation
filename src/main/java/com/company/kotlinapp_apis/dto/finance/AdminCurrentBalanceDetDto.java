73f4b338da0801700224f8c361ddbcdce647f68e
rovshan <rouvsen@gmail.com>
2023-06-26 01:35:56 +0400

    AdminCurrentBalanceDetDto added

---
+++ src/main/java/com/company/kotlinapp_apis/dto/finance/AdminCurrentBalanceDetDto.java
@@ -0,0 +1,18 @@
+package com.company.kotlinapp_apis.dto.finance;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class AdminCurrentBalanceDetDto {
+
+    private Long id;
+    private Integer price;
+    private String date;
+    private String time;
+}
